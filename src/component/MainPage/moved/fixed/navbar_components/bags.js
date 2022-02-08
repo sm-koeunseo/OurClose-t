668bd3c730313d4dc87dd75b19333d8fdae46be3
chominyeong <myc0104@naver.com>
2022-02-08 12:34:56 +0900

    0208 commit

--- src/component/MainPage/moved/fixed/navbar_components/bags.js
+++ src/component/MainPage/moved/fixed/navbar_components/bags.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 import Header from '../Header';
 import Navbar from '../Navbar';
-
+import './navbar_components.css';
+import noimg from './noimg.png'
 class Bags extends Component{
     render(){
         return(
             <div>
                 <Header/>
                 <Navbar/>
-                bags
+                <div style={{marginTop:"40px",marginBottom:"40px"}} className='noimg'>
+                    <img alt='noimg' src={noimg}/>
+                    <img alt='noimg' src={noimg}/> 
+                    <img alt='noimg' src={noimg}/> 
+                    <img alt='noimg' src={noimg}/>                    
+                </div>
+                <div style={{marginTop:"40px",marginBottom:"40px"}} className='noimg'>
+                    <img alt='noimg' src={noimg}/>
+                    <img alt='noimg' src={noimg}/> 
+                    <img alt='noimg' src={noimg}/> 
+                    <img alt='noimg' src={noimg}/>                    
+                </div>
             </div>
         )
     }
